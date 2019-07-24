ea073235f2d8df59ab49d53cf5eb50ebc7e19416
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-24 20:59:28 +0200

    non blocking selector server

---
+++ src/main/java/SingleThreadedSelectorNonBlockingServer.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.*;
+
+/**
+ * Created by mtumilowicz on 2019-07-24.
+ */
+public class SingleThreadedSelectorNonBlockingServer {
+
+    private final int port;
+
+    public SingleThreadedSelectorNonBlockingServer(int port) {
+        this.port = port;
+    }
+
+    public SingleThreadedSelectorNonBlockingServer() {
+        this.port = 81;
+    }
+
+    public static void main(String[] args) throws IOException {
+        new SingleThreadedSelectorNonBlockingServer().start();
+    }
+
+    public void start() throws IOException {
+        log("Creating server socket on port " + port);
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.bind(new InetSocketAddress("localhost", port));
+        ssc.configureBlocking(false);
+        log("Created server socket on port " + port);
+
+        Selector selector = Selector.open();
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+        Map<SocketChannel, Queue<ByteBuffer>> pendingData = new HashMap<>();
+        AcceptHandler acceptHandler = new AcceptHandler(pendingData);
+        ReadHandler readHandler = new ReadHandler(pendingData);
+        WriteHandler writeHandler = new WriteHandler(pendingData);
+
+        while (true) {
+            selector.select();
+            Set<SelectionKey> keys = selector.selectedKeys();
+            for (Iterator<SelectionKey> it = keys.iterator(); it.hasNext(); ) {
+                SelectionKey key = it.next();
+                it.remove();
+                if (key.isValid()) {
+                    if (key.isAcceptable()) {
+                        acceptHandler.handle(key);
+                    } else if (key.isReadable()) {
+                        readHandler.handle(key);
+                    } else if (key.isWritable()) {
+                        writeHandler.handle(key);
+                    }
+                }
+            }
+        }
+    }
+
+    private void handle(Runnable clientConnection) {
+        clientConnection.run();
+    }
+
+    private void log(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/WriteHandler.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * Created by mtumilowicz on 2019-07-24.
+ */
+public class WriteHandler {
+
+    private final Map<SocketChannel, Queue<ByteBuffer>> pendingData;
+
+    public WriteHandler(Map<SocketChannel, Queue<ByteBuffer>> pendingData) {
+        this.pendingData = pendingData;
+    }
+
+    public void handle(SelectionKey key) throws IOException {
+        SocketChannel sc = (SocketChannel) key.channel();
+        Queue<ByteBuffer> queue = pendingData.get(sc);
+        while(!queue.isEmpty()) {
+            ByteBuffer buf = queue.peek();
+            int written = sc.write(buf);
+            if (written == -1) {
+                sc.close();
+                pendingData.remove(sc);
+                return;
+            }
+            if (buf.hasRemaining()) {
+                return;
+            } else {
+                queue.remove();
+            }
+        }
+        key.interestOps(SelectionKey.OP_READ);
+    }
+}
