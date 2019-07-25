1bad0008af541c3f29bf80eedf3f7a389355b1de
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-25 23:31:03 +0200

    removing handlers

--- src/main/java/selector/SelectionKeyHandler.java
+++ src/main/java/selector/SelectionKeyHandler.java
@@ -3,10 +3,12 @@ package selector;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Created by mtumilowicz on 2019-07-25.
@@ -14,20 +16,65 @@ import java.util.Queue;
 public class SelectionKeyHandler {
     public static void handle(SelectionKey key) {
         Map<SocketChannel, Queue<ByteBuffer>> dataToHandle = new HashMap<>();
-        AcceptHandler acceptHandler = new AcceptHandler();
-        ReadHandler readHandler = new ReadHandler();
-        WriteHandler writeHandler = new WriteHandler();
 
         try {
             if (key.isAcceptable()) {
-                acceptHandler.handle(key, dataToHandle);
+                accept(key, dataToHandle);
             } else if (key.isReadable()) {
-                readHandler.handle(key, dataToHandle);
+                read(key, dataToHandle);
             } else if (key.isWritable()) {
-                writeHandler.handle(key, dataToHandle);
+                write(key, dataToHandle);
             }
         } catch (IOException e) {
             // workshops
         }
+    }
+
+    public static void accept(SelectionKey key, Map<SocketChannel, Queue<ByteBuffer>> dataToHandle) throws IOException {
+        ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+        SocketChannel sc = ssc.accept(); // never null, nonblocking
+        System.out.println("Someone connected: " + sc);
+        sc.configureBlocking(false);
+        dataToHandle.put(sc, new ConcurrentLinkedQueue<>());
+
+        sc.register(key.selector(), SelectionKey.OP_READ);
+    }
+
+    public static void read(SelectionKey key, Map<SocketChannel, Queue<ByteBuffer>> dataToHandle) throws IOException {
+        SocketChannel sc = (SocketChannel) key.channel();
+        ByteBuffer buf = ByteBuffer.allocateDirect(80);
+        int read = sc.read(buf);
+        if (read == -1) {
+            dataToHandle.remove(sc);
+            return;
+        }
+        if (read > 0) {
+            buf.flip();
+            for (int i = 0; i < buf.limit(); i++) {
+                buf.put(i, buf.get(i));
+            }
+            dataToHandle.get(sc).add(buf);
+            key.interestOps(SelectionKey.OP_WRITE);
+        }
+    }
+
+    public static void write(SelectionKey key, Map<SocketChannel, Queue<ByteBuffer>> dataToHandle) throws IOException {
+        SocketChannel sc = (SocketChannel) key.channel();
+        Queue<ByteBuffer> queue = dataToHandle.get(sc);
+        while(!queue.isEmpty()) {
+            ByteBuffer buf = queue.peek();
+            int written = sc.write(buf);
+            if (written == -1) {
+                sc.close();
+                dataToHandle.remove(sc);
+                return;
+            }
+            if (buf.hasRemaining()) {
+                return;
+            } else {
+                queue.remove();
+            }
+        }
+        key.interestOps(SelectionKey.OP_READ);
     }
 }
