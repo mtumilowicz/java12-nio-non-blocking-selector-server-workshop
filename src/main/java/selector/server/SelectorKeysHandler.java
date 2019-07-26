17abba48cbc2618508ffed082fa89ea3f00c502c
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-26 22:05:26 +0200

    SelectorKeysHandler.java

---
+++ src/main/java/selector/server/SelectorKeysHandler.java
@@ -0,0 +1,42 @@
+package selector.server;
+
+import selector.handler.AcceptHandler;
+import selector.handler.ReadHandler;
+import selector.handler.WriteHandler;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.*;
+
+/**
+ * Created by mtumilowicz on 2019-07-26.
+ */
+public class SelectorKeysHandler {
+    public static final void handle(Selector selector) throws IOException {
+        Map<SocketChannel, Queue<ByteBuffer>> pendingData = new HashMap<>();
+        AcceptHandler acceptHandler = new AcceptHandler(pendingData);
+        ReadHandler readHandler = new ReadHandler(pendingData);
+        WriteHandler writeHandler = new WriteHandler(pendingData);
+
+        while (true) {
+            selector.select();
+            Set<SelectionKey> keys = selector.selectedKeys();
+            for (Iterator<SelectionKey> it = keys.iterator(); it.hasNext(); ) {
+                SelectionKey key = it.next();
+                it.remove();
+                if (key.isValid()) {
+                    if (key.isAcceptable()) {
+                        acceptHandler.handle(key);
+                    } else if (key.isReadable()) {
+                        readHandler.handle(key);
+                    } else if (key.isWritable()) {
+                        writeHandler.handle(key);
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/java/selector/server/SingleThreadedSelectorNonBlockingServer.java
+++ src/main/java/selector/server/SingleThreadedSelectorNonBlockingServer.java
@@ -1,17 +1,10 @@
 package selector.server;
 
-import selector.handler.AcceptHandler;
-import selector.handler.ReadHandler;
-import selector.handler.WriteHandler;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import java.util.*;
 
 public class SingleThreadedSelectorNonBlockingServer {
 
@@ -26,27 +19,6 @@ public class SingleThreadedSelectorNonBlockingServer {
         Selector selector = Selector.open();
         ssc.register(selector, SelectionKey.OP_ACCEPT);
 
-        Map<SocketChannel, Queue<ByteBuffer>> pendingData = new HashMap<>();
-        AcceptHandler acceptHandler = new AcceptHandler(pendingData);
-        ReadHandler readHandler = new ReadHandler(pendingData);
-        WriteHandler writeHandler = new WriteHandler(pendingData);
-
-        while (true) {
-            selector.select();
-            Set<SelectionKey> keys = selector.selectedKeys();
-            for (Iterator<SelectionKey> it = keys.iterator(); it.hasNext(); ) {
-                SelectionKey key = it.next();
-                it.remove();
-                if (key.isValid()) {
-                    if (key.isAcceptable()) {
-                        acceptHandler.handle(key);
-                    } else if (key.isReadable()) {
-                        readHandler.handle(key);
-                    } else if (key.isWritable()) {
-                        writeHandler.handle(key);
-                    }
-                }
-            }
-        }
+        SelectorKeysHandler.handle(selector);
     }
 }
