54cbfd7e96f2aafb85b3b2d540e6645bed37eae0
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-31 23:19:32 +0200

    Selector extends XNBServer

--- src/main/java/selector/server/SelectorNonBlockingServerWithWorkerPool.java
+++ src/main/java/selector/server/SelectorNonBlockingServerWithWorkerPool.java
@@ -1,28 +1,25 @@
 package selector.server;
 
 import selector.handler.PooledSelectorKeysHandler;
-import server.XServer;
+import server.XNBServer;
 
 import java.io.IOException;
-import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
 
-public class SelectorNonBlockingServerWithWorkerPool extends XServer {
+public class SelectorNonBlockingServerWithWorkerPool extends XNBServer {
 
     public SelectorNonBlockingServerWithWorkerPool(int port) {
         super(port);
     }
 
-    public static void main(String[] args) throws IOException {
-        new SelectorNonBlockingServerWithWorkerPool(81).start();
+    @Override
+    protected void handleConnections(Selector selector) throws IOException {
+        new PooledSelectorKeysHandler().handle(selector);
+
     }
 
-    @Override
-    protected void processSockets(ServerSocketChannel ssc) throws IOException {
-        Selector selector = Selector.open();
-        ssc.register(selector, SelectionKey.OP_ACCEPT);
-        new PooledSelectorKeysHandler().handle(selector);
+    public static void main(String[] args) throws IOException {
+        new SelectorNonBlockingServerWithWorkerPool(81).start();
     }
 
     private void log(String message) {
--- src/main/java/selector/server/SingleThreadedSelectorNonBlockingServer.java
+++ src/main/java/selector/server/SingleThreadedSelectorNonBlockingServer.java
@@ -1,29 +1,24 @@
 package selector.server;
 
 import selector.handler.SelectorKeysHandler;
-import server.XServer;
+import server.XNBServer;
 
 import java.io.IOException;
-import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
 
-public class SingleThreadedSelectorNonBlockingServer extends XServer {
+public class SingleThreadedSelectorNonBlockingServer extends XNBServer {
 
     public SingleThreadedSelectorNonBlockingServer(int port) {
         super(port);
     }
 
-    public static void main(String[] args) throws IOException {
-        new SingleThreadedSelectorNonBlockingServer(81).start();
+    @Override
+    protected void handleConnections(Selector selector) throws IOException {
+        new SelectorKeysHandler().handle(selector);
     }
 
-    @Override
-    protected void processSockets(ServerSocketChannel ssc) throws IOException {
-        Selector selector = Selector.open();
-        ssc.register(selector, SelectionKey.OP_ACCEPT);
-
-        new SelectorKeysHandler().handle(selector);
+    public static void main(String[] args) throws IOException {
+        new SingleThreadedSelectorNonBlockingServer(81).start();
     }
 
     private void log(String message) {
