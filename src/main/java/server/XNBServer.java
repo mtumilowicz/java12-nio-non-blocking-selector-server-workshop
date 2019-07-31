fafefe28e2cd11dee3049872f3e930270d4a971d
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-07-31 23:19:18 +0200

    XNBServer

---
+++ src/main/java/server/XNBServer.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+/**
+ * Created by mtumilowicz on 2019-07-31.
+ */
+public abstract class XNBServer extends XServer {
+    public XNBServer(int port) {
+        super(port);
+    }
+
+    @Override
+    protected void processSockets(ServerSocketChannel ssc) throws IOException {
+        Selector selector = Selector.open();
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+        handleConnections(selector);
+    }
+    
+    protected abstract void handleConnections(Selector selector) throws IOException;
+}
