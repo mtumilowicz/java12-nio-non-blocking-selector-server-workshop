4bd9fb80dcc010b5364552e40e910758ff70ac72
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-08-03 17:29:19 +0200

    PooledReadHandlerAnswer.java, ReadHandlerAnswer.java - abstracting new class

--- src/main/java/selector/handler/answer/PooledReadHandlerAnswer.java
+++ src/main/java/selector/handler/answer/PooledReadHandlerAnswer.java
@@ -11,61 +11,28 @@ import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.function.UnaryOperator;
 
-class PooledReadHandlerAnswer {
+class PooledReadHandlerAnswer extends XReadHandlerAnswer {
     private final ExecutorService pool;
-    private final Map<SocketChannel, Queue<ByteBuffer>> pendingData;
     private final Queue<Runnable> selectorActions;
 
     PooledReadHandlerAnswer(ExecutorService pool,
                             Map<SocketChannel, Queue<ByteBuffer>> pendingData,
                             Queue<Runnable> selectorActions) {
+        super(pendingData);
         this.pool = pool;
-        this.pendingData = pendingData;
         this.selectorActions = selectorActions;
     }
 
-    void handle(SelectionKey key) throws IOException {
-        if (canBeRead(key)) {
-            ByteBuffer buf = ByteBuffer.allocateDirect(80);
-            int bytesRead = read(key, buf);
-            closeClientIfEnd(bytesRead, key);
-        }
-    }
-
-    private void switchToWrite(SelectionKey key) {
+    @Override
+    void switchToWrite(SelectionKey key) {
         selectorActions.add(() -> key.interestOps(SelectionKey.OP_WRITE));
         key.selector().wakeup();
     }
 
-    private int read(SelectionKey key, ByteBuffer buf) throws IOException {
-        SocketChannel client = (SocketChannel) key.channel();
-        int read = client.read(buf);
-        if (read > 0) {
-            pool.submit(() -> {
-                writeToBuffer(key, buf);
-            });
-        }
-
-        return read;
-    }
-
-    private void writeToBuffer(SelectionKey key, ByteBuffer buf) {
-        SocketChannel client = (SocketChannel) key.channel();
-        buf.flip();
-        BufferTransformer.transformBytes(buf, UnaryOperator.identity());
-        pendingData.get(client).add(buf);
-        switchToWrite(key);
-    }
-
-    private void closeClientIfEnd(int read, SelectionKey key) throws IOException {
-        if (read == -1) {
-            SocketChannel client = (SocketChannel) key.channel();
-            pendingData.remove(client);
-            client.close();
-        }
-    }
-
-    private boolean canBeRead(SelectionKey key) {
-        return key.isValid() && key.isReadable();
+    @Override
+    void xxx(SelectionKey key, ByteBuffer buf) {
+        pool.submit(() -> {
+            writeToBuffer(key, buf);
+        });
     }
 }
