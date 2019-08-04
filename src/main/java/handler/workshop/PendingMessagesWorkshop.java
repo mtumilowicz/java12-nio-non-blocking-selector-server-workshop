ae3f27a42d92693ea207ce47ed7aee9b07cfd0b4
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-08-04 23:01:33 +0200

    PendingMessagesWorkshop.java

--- src/main/java/handler/workshop/PendingMessagesWorkshop.java
+++ src/main/java/handler/workshop/PendingMessagesWorkshop.java
@@ -8,14 +8,13 @@ import java.nio.channels.SocketChannel;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.UnaryOperator;
 
 class PendingMessagesWorkshop {
     private final Map<SocketChannel, Queue<ByteBuffer>> pendingMessagesByClient = new ConcurrentHashMap<>();
 
     void initFor(SocketChannel client) {
-        pendingMessagesByClient.put(client, new ConcurrentLinkedQueue<>());
+        // entry for that client with thread-safe collection, hint: ConcurrentLinkedQueue
     }
 
     void sendTo(SocketChannel client) throws IOException {
@@ -30,17 +29,17 @@ class PendingMessagesWorkshop {
     }
 
     void closeClientIfEnd(SocketChannel client) throws IOException {
-        pendingMessagesByClient.remove(client);
-        client.close();
+        // remove from collection
+        // close client, hint: client.close()
     }
 
     void prepareForSendingTo(SocketChannel client, ByteBuffer buffer) {
         prepareBuffer(buffer);
-        pendingMessagesByClient.get(client).add(buffer);
+        // add to the queue for the given client
     }
 
     private void prepareBuffer(ByteBuffer buf) {
-        buf.flip();
+        // reverse the buffer, hint: buf.flip()
         BufferTransformer.transformBytes(buf, UnaryOperator.identity());
     }
 }
