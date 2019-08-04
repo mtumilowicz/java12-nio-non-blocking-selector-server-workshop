f00a28b5284876de20a6203c7a9f0e7f8f3572d7
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-08-04 11:18:16 +0200

    WriteHandlerWorkshop.java hints

--- src/main/java/selector/handler/workshop/WriteHandlerWorkshop.java
+++ src/main/java/selector/handler/workshop/WriteHandlerWorkshop.java
@@ -16,13 +16,14 @@ class WriteHandlerWorkshop {
         if (canBeWritten(key)) {
             // get client (SocketChannel) from key, hint: key.channel() + casting
             SocketChannel client = null;
-            pendingMessages.sendTo(client);
+            // send pending messages to that client, hint: pendingMessages.sentTo(client)
             // switch key to listen to read again, hint: key.interestOps(...), OP_READ
-            key.interestOps(SelectionKey.OP_READ);
         }
     }
 
     private boolean canBeWritten(SelectionKey key) {
-        return key.isValid() && key.isWritable();
+        // key must be valid and must be ready to write
+        // hint: isValid(), isWritable()
+        return false;
     }
 }
